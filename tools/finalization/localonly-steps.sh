15da9f7fd4137723fcfb331f994097c719899d54
someone5678 <someone5678@users.noreply.github.com>
2024-06-16 15:15:44 +0900

    tools: Update localonly-steps.sh
    * Generate prebuilt sdk files
    * Still required some works after build...
    * Built files will be located under out/dist/mainline-sdks/for-<version-number>-build
    
    Change-Id: I7c27efa8dc5b47a8845ecc2510e18e07b9cd6e05
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>

--- tools/finalization/localonly-steps.sh
+++ tools/finalization/localonly-steps.sh
@@ -3,21 +3,17 @@
 set -ex
 
 function finalize_locally() {
-    local top="$(dirname "$0")"/../../../..
+    local top="../../"
     source $top/build/make/tools/finalization/environment.sh
 
     # default target to modify tree and build SDK
-    local m="$top/build/soong/soong_ui.bash --make-mode TARGET_PRODUCT=aosp_arm64 TARGET_RELEASE=fina_1 TARGET_BUILD_VARIANT=userdebug DIST_DIR=out/dist"
-
-    # adb keys
-    $m adb
-    LOGNAME=android-eng HOSTNAME=google.com "$top/out/host/linux-x86/bin/adb" keygen "$top/vendor/google/security/adb/${FINAL_PLATFORM_VERSION}.adb_key"
+    local m="$top/build/soong/soong_ui.bash --make-mode TARGET_PRODUCT=aosp_arm64 TARGET_RELEASE=ap2a TARGET_BUILD_VARIANT=user DIST_DIR=out/dist"
 
     # Build Platform SDKs.
-    $top/build/soong/soong_ui.bash --make-mode TARGET_PRODUCT=sdk TARGET_RELEASE=fina_1 TARGET_BUILD_VARIANT=userdebug sdk dist sdk_repo DIST_DIR=out/dist
+    # $top/build/soong/soong_ui.bash --make-mode TARGET_PRODUCT=sdk TARGET_RELEASE=ap2a TARGET_BUILD_VARIANT=user sdk dist sdk_repo DIST_DIR=out/dist
 
     # Build Modules SDKs.
-    TARGET_RELEASE=fina_1 TARGET_BUILD_VARIANT=userdebug UNBUNDLED_BUILD_SDKS_FROM_SOURCE=true DIST_DIR=out/dist "$top/vendor/google/build/mainline_modules_sdks.sh" --build-release=latest
+    TARGET_RELEASE=ap2a TARGET_BUILD_VARIANT=user UNBUNDLED_BUILD_SDKS_FROM_SOURCE=true DIST_DIR=out/dist "$top/packages/modules/common/build/mainline_modules_sdks.sh" --build-release=UpsideDownCake
 
     # Update prebuilts.
     "$top/prebuilts/build-tools/path/linux-x86/python3" -W ignore::DeprecationWarning "$top/prebuilts/sdk/update_prebuilts.py" --local_mode -f ${FINAL_PLATFORM_SDK_VERSION} -e ${FINAL_MAINLINE_EXTENSION} --bug 1 1
